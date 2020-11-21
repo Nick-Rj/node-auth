6fa427a5058d69999f0974545c3cbbc7652b84e0
Nick-Rj <nik.rj10348@gmail.com>
2020-11-21 21:05:24 +0530

    Basic API

---
+++ routes/api/users.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const router=express.Router();
+
+
+// means - /api/users/
+router.get('/', (req,res)=>{
+    return res.status(200).json({
+        success:true,
+    });
+
+});
+
+// means - /api/users/login/
+router.get('/login', ()=>{});
+
+// means - /api/users/signup/
+router.get('signup', ()=>{});
+
+module.exports=router;
+
+
+
+
